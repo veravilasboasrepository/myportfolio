3b2b4cba0901d1147e9eaa1d79037983e06368f7
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-04-22 17:15:34 +0100

    several changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./Navbar";
+import 
 
+export default App;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/035/666/737/non_2x/black-minimalist-logo-sample-for-brands-on-a-transparent-background-free-png.png"
+          className="logo img-fluid"
+          alt="logo"
+        />
       </header>
+      <Navbar />
+      <div className="container">
+        <h1>👋 Hello, I'm</h1>
+        <h1 className="mb-5"> Vera Vilas Boas</h1>
+        <h2 className="mb-5"> Front end developer from portugal</h2>
+        <a href="/contact.html" class="btn btn-branding-outline">
+          Contact me
+        </a>
+      </div>
+      <div class="container">
+        <div class="row mt-5 mb-5">
+          <div className="col d-none d-lg-block">
+            <img
+              src="images/39a386180976633.Y3JvcCwxMTc2LDkyMCw4Nyww.png"
+              className="img-fluid rounded"
+              alt="image porfolio"
+            />
+          </div>
+          <div className="col">
+            <div className="project-desciption">
+              <h2 className="mb-5">XX project</h2>
+              <p className="mb-5 text-muted">
+                Far far away, behind the word mountains, far from the countries
+                Vokalia and Consonantia, there live the blind texts. Separated
+                they live in Bookmarksgrove right at the coast of the Semantics,
+                a large language ocean. A small river named Duden flows by
+              </p>
+              <a href="" className="btn btn-branding-outline">
+                {" "}
+                Learn more{" "}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <main> </main>
+      <footer className="container">
+        <div className="contact-box justify-content-between mb-4 d-none d-md-flex">
+          <div>
+            <h4>Work Inquiry</h4>
+            <p className="text-muted">Let's work together</p>
+          </div>
+          <div>
+            <a href="" class="btn btn-branding mt-3">
+              {" "}
+              Contact me{" "}
+            </a>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center mb-5">
+          <a href="mailto:veravilasboas@outlook.pt" class="email-link">
+            VeraVilasBoas
+          </a>
+        </div>
+        <div className="social-links d-flex justify-content-center">
+          <a
+            href="https://github.com/Verahvilasboas/my-portfolio"
+            target="_blank"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.instragram.com/Verah" target="_blank">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.twitter.com/Vera" target="_blank">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
-
-export default App;
---
+++ my-app/src/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Navbar.css"
+
+
+export default function Navbar(props) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div className="container">
+    
+        <a className="navbar-brand" href="/" title="Homepage">Vera Vilas Boas </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="active nav-item">
+              <a
+                className="nav-link active"
+                href="./App.js"
+                > Home </a >
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./About.js"
+                >About me</a
+              >
+            </li>
+               <li className="nav-item">
+              <a className="nav-link" href="./Project.js"> Projects </a
+              >
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="./About.js"
+                >Contact me</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
